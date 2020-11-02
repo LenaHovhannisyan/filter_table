f66bd5d6fb6870c4075978ac161b379819aa62f4
LenaHovhannisyan <hovhannisyan.lena17@gmail.com>
2020-11-02 12:42:27 +0400

    Delete password from db config

--- app/db.js
+++ app/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'test1234',
+    password: '',
     database: 'table_db'
 });
 
